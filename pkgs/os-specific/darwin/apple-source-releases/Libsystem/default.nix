3c5b09a83674eda6dc0b378917e807a07537d8b7
Rune K. Svendsen <runesvend@gmail.com>
2024-03-29 11:54:38 +0100

    Libsystem: remove broken `postFetch` step in `darling.src`
    The `fetchzip` `postFetch` step in `darling.src` in `Libsystem` is broken because it references `$downloadedFile`, which does not exist because `fetchzip` moves `$downloadedFile` to `$renamed` before the `postFetch` step is executed.
    
    The brokenness of this step did not become apparent until commit 5a6a31e54ddaf59aa8e1c5e7e9b02623d4881c34 was merged, since before this commit the `fetchzip` `postFetch` step wasn't executed at all.
    
    Furthermore, the tarball referenced by `darling.src` no longer contains both `src/opendirectory` and `src/OpenDirectory`, so the `postFetch` step isn't needed anyway.

--- pkgs/os-specific/darwin/apple-source-releases/Libsystem/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/Libsystem/default.nix
@@ -10,25 +10,7 @@
 let
   darling.src = fetchzip {
     url = "https://github.com/darlinghq/darling/archive/d2cc5fa748003aaa70ad4180fff0a9a85dc65e9b.tar.gz";
-    sha256 = "11b51fw47nl505h63bgx5kqiyhf3glhp1q6jkpb6nqfislnzzkrf";
-    postFetch = ''
-      # The archive contains both `src/opendirectory` and `src/OpenDirectory`,
-      # pre-create the directory to choose the canonical case on
-      # case-insensitive filesystems.
-      mkdir -p $out/src/OpenDirectory
-
-      cd $out
-      tar -xzf $downloadedFile --strip-components=1
-      rm -r $out/src/libm
-
-      # If `src/opendirectory` and `src/OpenDirectory` refer to different
-      # things, then combine them into `src/OpenDirectory` to match the result
-      # on case-insensitive filesystems.
-      if [ "$(stat -c %i src/opendirectory)" != "$(stat -c %i src/OpenDirectory)" ]; then
-        mv src/opendirectory/* src/OpenDirectory/
-        rmdir src/opendirectory
-      fi
-    '';
+    sha256 = "sha256-n6cAOGgOu0S5jYjniTwZxXiZPPz6prUffPw6Fvy3bNY=";
   };
 
   # Libsystem needs `asl.h` from syslog. This is the version corresponding to the 10.12 SDK
