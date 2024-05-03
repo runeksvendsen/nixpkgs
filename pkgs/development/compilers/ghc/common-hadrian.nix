b574a891f9d1172e455234b282774d588cd5c150
Rune K. Svendsen <runesvend@gmail.com>
2024-05-03 11:53:39 +0200

    GHC three-way-sort patch

--- pkgs/development/compilers/ghc/common-hadrian.nix
+++ pkgs/development/compilers/ghc/common-hadrian.nix
@@ -274,6 +274,7 @@ stdenv.mkDerivation ({
     (if lib.versionAtLeast version "9.8"
       then ./docs-sphinx-7-ghc98.patch
       else ./docs-sphinx-7.patch )
+    ./three-way-sort.patch
   ] ++ lib.optionals (stdenv.targetPlatform.isDarwin && stdenv.targetPlatform.isAarch64) [
     # Prevent the paths module from emitting symbols that we don't use
     # when building with separate outputs.
